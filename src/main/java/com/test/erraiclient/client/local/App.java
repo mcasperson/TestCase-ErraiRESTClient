82de0783b9046794c0a57cc2e07449488cf395c7
Matthew Casperson <mcaspers@redhat.com>
2012-06-12 14:36:44 +1000

    Initial Import

---
+++ src/main/java/com/test/erraiclient/client/local/App.java
@@ -0,0 +1,79 @@
+package com.test.erraiclient.client.local;
+
+import javax.annotation.PostConstruct;
+
+import org.jboss.errai.bus.client.api.ErrorCallback;
+import org.jboss.errai.bus.client.api.Message;
+import org.jboss.errai.bus.client.api.RemoteCallback;
+import org.jboss.errai.enterprise.client.jaxrs.api.RestClient;
+import org.jboss.errai.ioc.client.api.EntryPoint;
+
+import com.test.restserver.RESTObject;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * Example code showing how to use Errai-JAXRS.
+ * 
+ * @author Christian Sadilek <csadilek@redhat.com>
+ */
+@EntryPoint
+public class App
+{
+	private static final String REST_SERVER = "http://localhost:8080/TestRESTServer/rest";
+
+	final RemoteCallback<RESTObject> callback = new RemoteCallback<RESTObject>()
+	{
+		@Override
+		public void callback(final RESTObject restObject)
+		{
+			Window.alert("Success! Value is " + restObject.getValue());
+		}
+	};
+	
+	final RemoteCallback<String> stringCallback = new RemoteCallback<String>()
+	{
+		@Override
+		public void callback(final String restObject)
+		{
+			Window.alert("Success! Value is " + restObject);
+		}
+	};
+
+	final ErrorCallback errorCallback = new ErrorCallback()
+	{
+
+		@Override
+		public boolean error(final Message message, final Throwable throwable)
+		{
+			throwable.printStackTrace();
+			Window.alert("Failure!");			
+			return true;
+		}
+	};
+
+	@PostConstruct
+	public void init()
+	{
+		/* Init the REST service */
+		RestClient.setApplicationRoot(REST_SERVER);
+		RestClient.setJacksonMarshallingActive(true);
+
+		final Button go = new Button("Go", new ClickHandler()
+		{
+			@Override
+			public void onClick(ClickEvent clickEvent)
+			{
+				//final REST restMethod = RestClient.create(REST.class, callback, errorCallback);
+				final RESTTwo restMethod = RestClient.create(RESTTwo.class, stringCallback, errorCallback);
+				restMethod.printMessage();
+			}
+		});
+
+		RootPanel.get().add(go);
+	}
+}
---
+++ src/main/java/com/test/erraiclient/client/local/REST.java
@@ -0,0 +1,20 @@
+package com.test.erraiclient.client.local;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.test.restserver.RESTObject;
+
+@Path("/")
+public interface REST
+{
+	@GET
+	@Path("test")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(
+	{ "*" })
+	public RESTObject printMessage();
+}
